39d72bf323be911091794ad0e5a554f716334f64
toxicGuru <besartmahmuti5@gmail.com>
2023-03-16 18:26:31 +0100

    gitIgnore added nodemon.sjon added config fixed

--- app.ts
+++ app.ts
@@ -1,11 +1,9 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, Express } from 'express';
+
 import publicRouter from './routes/public.routes'
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app: Express = express();
 
 app.use('/', publicRouter)
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}.`);
-});
+export default app;
---
+++ bin/index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import app from '../app'
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}.`);
+  });
+  
